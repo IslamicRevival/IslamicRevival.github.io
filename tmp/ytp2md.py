9b03e502bc31615b6b99670cccccd3d77abd1d60
SirVival <admin@nikolav.rs>
2022-12-24 12:40:52 +0000

    search all vids in a channel

--- tmp/ytp2md.py
+++ tmp/ytp2md.py
@@ -18,24 +18,50 @@ from pyyoutube import Api
 @click.option('-c','--channel-id')
 #def main(channel_id="UC0uyPbeJ56twBLoHUbwFKnA"):
 def main(channel_id="UC57cqHgR_IZEs3gx0nxyZ-g"):
-    api = Api(api_key="AIzaSyABaeCa_GEW4ePYNfYwP9qtsHAMN8s8kxs")
-    
+    #api = Api(api_key="AIzaSyABaeCa_GEW4ePYNfYwP9qtsHAMN8s8kxs")
+    api = Api(api_key="AIzaSyBdbQ-WPIkQkEad2EtRPfbRMiMURPxyqm8")  # Google Data (YouTube v3 key)
+    channel_id = "UC57cqHgR_IZEs3gx0nxyZ-g"
+
     print ("\tFetch all the playlists")
     playlists_by_channel = api.get_playlists(channel_id=channel_id,count=None)
     print("\tFetch all the videos of the playlist")
-    playlists_videos = []
-    for playlist in playlists_by_channel.items:
-        print("\t\tFetching videos IDs of playlist %s" %(playlist.id))
-        playlists_videos.append(api.get_playlist_items(playlist_id=playlist.id,count=None))
+    # playlists_videos = []
+    # for playlist in playlists_by_channel.items:
+    #     print("\t\tFetching videos IDs of playlist %s" %(playlist.id))
+    #     playlists_videos.append(api.get_playlist_items(playlist_id=playlist.id,count=None))
+
+    # videos_ids = []
+    # for playlist in playlists_videos:
+    #     for video in playlist.items:
+    #         videos_ids.append(video.snippet.resourceId.videoId) 
+    # print("We gathered now %s videos, saving save to file" %(len(videos_ids)))
+    # with open("channel_id_file",'w') as f:
+    #     json.dump(videos_ids,f)
 
     videos_ids = []
-    for playlist in playlists_videos:
-        for video in playlist.items:
-            videos_ids.append(video.snippet.resourceId.videoId) 
-    print("We gathered now %s videos, saving save to file" %(len(videos_ids)))
-    with open("channel_id_file",'w') as f:
-        json.dump(videos_ids,f)
-        
+    limit = 2
+    count = 1
+    try:
+        print('Starting Fetching the Data for Channel:', channel_id)
+        response = api.search(channel_id=channel_id, limit=limit, count=count)
+        next_page_token = response.nextPageToken
+        while next_page_token:
+            for res in response.items:
+                if res.id.videoId:
+                    videos_ids.append(res.id.videoId)
+                    print(res.id.videoId)
+
+            next_page_token = response.nextPageToken
+            response = api.search(
+                channel_id=channel_id,
+                limit=limit,
+                count=count,
+                page_token=next_page_token
+            )
+    except Exception as e:
+        print('Error Getting Data:', e)
+    print(videos_ids)
+    
     print("Save %s channel videos transcripts" % (channel_id) )
     
     for video_id in videos_ids:
--- yt2md.py
+++ yt2md.py
@@ -4,9 +4,7 @@ import datetime
 import logging
 import os
 import json
-import re
 
-import click
 import requests
 
 from py_youtube import Data ## doesn't need API key
@@ -54,8 +52,7 @@ def get_caption_markdown(video_id: str):
                                                                       text=c["text"]) for c in captions
     ]
 
-def get_preview_image(img_url: str, video_id: str, path: str):
-    img_file_name = os.path.join(path, video_id) + '.jpg'
+def get_preview_image(img_file_name:str, img_url: str, video_id: str, path: str):
     with open(img_file_name, 'wb') as handle:
         headers = {'Content-Type': 'application/json','Referer': '*.[my-app].appspot.com/*'}
         response = requests.get(img_url, stream=True, headers=headers)
@@ -68,10 +65,7 @@ def get_preview_image(img_url: str, video_id: str, path: str):
             if not block:
                 break
             handle.write(block)
-
         return img_file_name
-
-    return None
 
 
 def gen_markdown_page(video_id: str, title: str, description: str, path: str, date: datetime, captions: list):
@@ -105,17 +99,6 @@ def string_to_filename(filename, raw=False):
     return filename
 
 
-@click.group()
-@click.option('-v', '--verbosity', help='Verbosity', default=0, count=True)
-def cli(verbosity: int):
-    _configure_logging(verbosity)
-    return 0
-
-@cli.command()
-#@click.option('-i', '--video_id', help='url of the youtube video')
-@click.option('-d', '--path', help='dir to output the markdown and thumbnail', default="content/transcripts")
-@click.option('-c', '--channel_id', help='get all vids in a channel', default="UC0uyPbeJ56twBLoHUbwFKnA")
-
 def get_transcript_by_vid_id(video_id: str, path: str, channel_id: str):
     api = Api(api_key="AIzaSyABaeCa_GEW4ePYNfYwP9qtsHAMN8s8kxs")
     video_metadata = api.get_video_by_id(video_id=video_id).items[0]
@@ -146,37 +129,63 @@ def get_transcript_by_vid_id(video_id: str, path: str, channel_id: str):
 def main(channel_ids=CHANNEL_ID):
 
     for channel_id in channel_ids:
-        api = Api(api_key="AIzaSyABaeCa_GEW4ePYNfYwP9qtsHAMN8s8kxs")
-        path="content/transcripts/"
-        print ("\tFetch all the playlists")
-        playlists_by_channel = api.get_playlists(channel_id=channel_id,count=None)
-        print("\tFetch all the videos of the playlist")
-        playlists_videos = []
-        for playlist in playlists_by_channel.items:
-            print("\t\tFetching videos IDs of playlist %s" %(playlist.id))
-            playlists_videos.append(api.get_playlist_items(playlist_id=playlist.id,count=None))
+        #api = Api(api_key="AIzaSyABaeCa_GEW4ePYNfYwP9qtsHAMN8s8kxs")
+        api = Api(api_key="AIzaSyBdbQ-WPIkQkEad2EtRPfbRMiMURPxyqm8")  # Google Data (YouTube v3 key)
 
-        videos_ids = []
-        for playlist in playlists_videos:
-            for video in playlist.items:
-                videos_ids.append(video.snippet.resourceId.videoId) 
-        print("We gathered now %s videos, saving save to file" %(len(videos_ids)))
-        with open("yt2md-channel_id_file",'w') as f:
-            json.dump(videos_ids,f)
-            
-        print("Save %s channel videos transcripts" % (channel_id) )
+        path="content/transcripts/"
+        # print ("\n\n\tFetch all the playlists")
+        # playlists_by_channel = api.get_playlists(channel_id=channel_id,count=None)
+        # print("\tFetch all the videos of the playlist")
+        # playlists_videos = []
+        # for playlist in playlists_by_channel.items:
+        #     print("\t\tFetching videos IDs of playlist %s" %(playlist.id))
+        #     playlists_videos.append(api.get_playlist_items(playlist_id=playlist.id,count=None))
+
         
+        # for playlist in playlists_videos:
+        #     for video in playlist.items:
+        #         videos_ids.append(video.snippet.resourceId.videoId) 
+        # vid_count = len(videos_ids)
+        # print(f"Gathered {vid_count} videos for {channel_id} saving to json file" )
+        # with open("yt2md-channel_id_file",'w') as f:
+        #     json.dump(videos_ids,f)
+        videos_ids = []
+        limit = 100
+        count = 25
+        try:
+            print(f"Fetching all vids in channel {channel_id}")
+            response = api.search(channel_id=channel_id, limit=limit, count=count)
+            next_page_token = response.nextPageToken
+            while next_page_token:
+                for res in response.items:
+                    if res.id.videoId:
+                        videos_ids.append(res.id.videoId)
+                        #print(res.id.videoId)
+                next_page_token = response.nextPageToken
+                response = api.search(
+                    channel_id=channel_id,
+                    limit=limit,
+                    count=count,
+                    page_token=next_page_token
+                )
+        except Exception as e:
+            print('Error getting vids for channel:', e)
+        vid_count = len(videos_ids)
+        print(f"Gathered {vid_count} videos for {channel_id} now pulling metadata for each video" )
+
         for video_id in videos_ids:
-            print ("The video ID is %s" % (video_id))
             try:
                 video_metadata = api.get_video_by_id(video_id=video_id).items[0]
-                title = video_metadata.snippet.title
-                preview_path = get_preview_image(img_url=video_metadata.snippet.thumbnails.default.url, video_id=video_id, path=path)
+                img_file_name = os.path.join(path, video_id) + '.jpg'
 
                 # check if video was already downloaded
-                if os.path.exists(preview_path):
-                        print("video was already downloaded because thumbnail already exists, skipping")
-                    # return
+                if os.path.exists(img_file_name):
+                    print(f"{video_id} already downloaded, skipping. ", end="")
+                    continue
+                preview_path = get_preview_image(img_file_name=img_file_name, img_url=video_metadata.snippet.thumbnails.default.url, video_id=video_id, path=path)
+
+                title = video_metadata.snippet.title
+                print(f"\n\nVideo ID is {video_id} with title {title}")
 
                 description = video_metadata.snippet.description
                 date = datetime.datetime.strptime(video_metadata.snippet.publishedAt, "%Y-%m-%dT%H:%M:%S%z")
@@ -194,7 +203,7 @@ def main(channel_ids=CHANNEL_ID):
             except Exception as e:
                 print(e)
 
-        print(f"Finished {channel_id}")
+        print(f"Finished processing videos and md for {channel_id}")
     
 
 if __name__ == '__main__':
