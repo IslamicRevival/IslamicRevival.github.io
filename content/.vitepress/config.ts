ada2859d2698d00a870023b03b047b8fff5a4b18
SirVival <admin@nikolav.rs>
2023-01-06 12:40:58 +0000

    fixed chrome crashing

--- content/.vitepress/config.ts
+++ content/.vitepress/config.ts
@@ -46,7 +46,7 @@ export default defineConfig({
   {
 		text: 'Blogging Theology',
 		items: [
-			{ text: 'Video summaries', link: '/blogging_theology/_Pagan_influences__on_the_Christian_Gospels_&_Paul.md' }
+			{ text: 'Video summaries', link: '/blogging_theology/The_Life_of_Muhammad__Chapter_11__A_Pact_of_Chivalry.md' }
 		],
 	},
   {
